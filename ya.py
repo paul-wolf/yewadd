7d5f81ec2763ea99db1e2e9590feb9d9dd2b8c9c
Paul Wolf <paul@bitposter.co>
2021-07-07 10:26:16 +0200

    Extend search spec

--- ya.py
+++ ya.py
@@ -1,11 +1,21 @@
-from typing import List, Optional, Dict
+import os
+import sys
+
+from typing import List, Dict
 import getpass
 import sqlite3
 
 import click
 
+import ipdb
 
 
+BASE_DIR = f"/Users/{getpass.getuser()}/Library/Application Support/AddressBook/Sources/"
+DB_NAME = "AddressBook-v22.abcddb"
+def get_db_path(base_path):
+    """Take the first directory in the base_dir."""
+    dirs = os.listdir(base_path)
+    return os.path.join(BASE_DIR, dirs.pop(), DB_NAME)
 
 PATH_DB = "/Users/paul/Library/Application Support/AddressBook/Sources/692171AE-40FF-4D24-8DF5-3FC8A986925A/AddressBook-v22.abcddb"
 
@@ -18,10 +28,25 @@ SQL_FIND = """SELECT
 FROM ZABCDRECORD r 
     LEFT JOIN ZABCDPHONENUMBER p ON r.z_pk = p.zowner  
     LEFT JOIN ZABCDEMAILADDRESS a ON r.z_pk = a.zowner  
-    
-WHERE 
-    r.zlastname LIKE ?
+"""
+
+SQL_FIND_WHERE_EXCLUDE = """ 
+
+WHERE
+    (r.zlastname LIKE ?
+    OR r.zfirstname LIKE ?)
     AND r.z_pk NOT IN (SELECT uid FROM exclude_list)
+"""
+
+SQL_FIND_WHERE_INCLUDE = """ 
+
+WHERE
+    (r.zlastname LIKE ?
+    OR r.zfirstname LIKE ?)
+"""
+
+SQL_FIND_ORDER = """
+
 ORDER BY 
     r.zlastname, r.zfirstname
 """
@@ -39,7 +64,11 @@ WHERE
     r.z_pk = ?
 """
 
-import ipdb
+def get_search_sql(include=False):
+    sql = SQL_FIND
+    sql += SQL_FIND_WHERE_INCLUDE if include else SQL_FIND_WHERE_EXCLUDE
+    sql += SQL_FIND_ORDER
+    return sql
 
 def dict_factory(cursor, row) -> Dict:
     d = dict()
@@ -47,9 +76,6 @@ def dict_factory(cursor, row) -> Dict:
         d[col[0]] = row[idx]
     return d
 
-
-conn = sqlite3.connect(PATH_DB)
-conn.row_factory = dict_factory
 
 def print_address(a):
     print(f'{a["uid"]}, {a["ZLASTNAME"]}, {a["ZFIRSTNAME"]}, {a["ZFULLNUMBER"]}, {a["ZADDRESS"]}')
@@ -96,12 +122,17 @@ def cli(ctx, user, debug):
     username = user or getpass.getuser()
     ctx.ensure_object(dict)
     ctx.obj["debug"] = debug
-    
+    db_path = get_db_path(BASE_DIR)
+    if not os.path.exists(db_path):
+        print(f"Could not find database at path: \n\t{db_path}")
+        raise SystemExit
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = dict_factory
+    ctx.obj["conn"] = conn    
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS exclude_list (uid data_type PRIMARY KEY)")
     
-def insert_exclude(cursor, uid):
-
+def insert_exclude(conn, cursor, uid):
     cursor.execute("SELECT uid FROM exclude_list WHERE uid = ?", [uid])
     entry = cursor.fetchone()
     if entry is None:
@@ -112,20 +143,21 @@ def insert_exclude(cursor, uid):
 @click.pass_context
 @click.argument("spec", required=False)
 @click.option("--query-exclude", "-d", is_flag=True, help="Query each entry if it should be listed", required=False)
-def ls(ctx, spec, query_exclude):
+@click.option("--include", "-i", is_flag=True, help="Include excluded contact records", required=False)
+def ls(ctx, spec, query_exclude, include):
     """List address entries with search spec."""
     
     spec = spec or ""
     spec += "%"
-    
+    conn = ctx.obj.get("conn")
     cur = conn.cursor()
-    cur.execute(SQL_FIND, [spec])
+    cur.execute(get_search_sql(include), [spec, spec])
     addresses = gather_addresses(cur)
     for uid, contact in addresses.items():
         print_address_dict(contact)
         if query_exclude:
             if click.confirm('Exclude from future queries?'):
-                insert_exclude(cur, uid)
+                insert_exclude(conn, cur, uid)
                 print(f"Excluding this address in future: {uid}")
         
 @cli.command()
@@ -134,7 +166,7 @@ def ls(ctx, spec, query_exclude):
 def exclude(ctx, uid):
     """Tell us to not include the record with uid in future 
     listings."""
-    
+    conn = ctx.obj.get("conn")    
     cur = conn.cursor()
     cur.execute(SQL_BY_UID, [uid])
     addresses = gather_addresses(cur)
@@ -143,7 +175,6 @@ def exclude(ctx, uid):
     for k, v in addresses.items():
         print_address_dict(v)
     
-# stats
 
 # include
 
@@ -151,7 +182,7 @@ def exclude(ctx, uid):
 @click.pass_context
 def info(ctx):
     """Print simple stats."""
-    
+    conn = ctx.obj.get("conn")
     cur = conn.cursor()
     sql = """SELECT count(*) as cnt FROM ZABCDRECORD
         WHERE z_pk NOT IN (SELECT uid FROM exclude_list)"""
@@ -163,6 +194,7 @@ def info(ctx):
     cur.execute(sql)
     cnt = cur.fetchone().get("cnt")
     print(f"Total records excluded: {cnt}")
+    print(f"Database: {get_db_path(BASE_DIR)}")
 
     
 
